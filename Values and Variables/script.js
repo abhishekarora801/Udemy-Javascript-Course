e5f083d46c60fee3edc7b00c64a251b6357543f3
username <abhishekarora801@gmail.com>
2022-10-11 16:58:36 +0530

    Values and Variables

---
+++ Values and Variables/script.js
@@ -0,0 +1,18 @@
+let js = 'amazing';
+
+let firstName = "Abhishek";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// underscore (e.g. _function) and dollar (e.g. $fucntion) are only 2 keywords, which are allowed besides letters in js.
+let _function = "Underscore is allowed before reserved keywords";
+let $function = "Dollar is allowed before reserved keywords";
+
+let Person = "jonas"; // we reserve capitals for OOP, so don't use it now.
+let PI = 3.1415;
+
+let myFirstJob = 'System Engineer Trainee';
+let myCurrentJob = 'Programmer Analyst';
+
+console.log(myCurrentJob);
