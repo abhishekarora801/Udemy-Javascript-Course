7691e92c0bba11ca8e326a93fec356f298430e32
username <abhishekarora801@gmail.com>
2022-10-11 18:41:04 +0530

    Understanding variables in js

---
+++ Let, const & var/script.js
@@ -0,0 +1,22 @@
+let age = 30;
+//re-assign or mutated the age variable
+age = 31;
+//to mutate, we use let
+
+// const -> variable that cannot be mutated.
+const birthYear = 1995;
+
+//we cannot use empty const variable at declaration, have to give them a value;
+//  e.g. const job;
+
+//Using var
+var job = 'Programmer';
+job = 'Pilot';
+
+//let is block-scoped and var is function-scoped. --> VERY IMPORTANT
+
+//Exercise
+let language = 'Hindi';
+const birthPlace = 'Delhi';
+const birthHospital = 'St. Stephens';
+language = 'English';
