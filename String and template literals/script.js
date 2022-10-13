86d11c0c06fedd4656888e8a3e0d5d3813a0e780
username <abhishekarora801@gmail.com>
2022-10-14 01:05:37 +0530

    String and template literals

---
+++ String and template literals/script.js
@@ -0,0 +1,22 @@
+const firstName = 'Abhishek';
+const job = 'Programmer';
+const birthYear = 1996;
+const year = 2022;
+
+const fullSentence = `I'm ${firstName}, a ${year - birthYear} years old ${job}!`;
+console.log(fullSentence);
+
+console.log(`String
+with
+multiple
+lines`);
+
+//Exercise
+
+const country = `India`;
+const continent = `Asia`;
+const population = `11 million`;
+const language = `Hindi`;
+
+const description = `${country} is in ${continent}, and its ${population} people speak ${language}!`;
+console.log(description);
