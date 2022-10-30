af536d3edecf7ee1f2b44b6bb4255f44e6e80bf2
username <abhishekarora801@gmail.com>
2022-10-30 20:16:00 +0530

    Intro to objects

---
+++ JavaScript Fundamentals - Part 2/Introduction to Objects/script.js
@@ -0,0 +1,30 @@
+'use strict';
+
+//creating array
+const jonasArray = [
+    'Jonas',
+    'Smith',
+    2037 - 1996,
+    'teacher',
+    ['Michael', 'Steven', 'Peter']
+];
+console.log(jonasArray);
+
+//creating object
+const jonasObject = {
+    firstName: 'Abhishek',
+    lastName: 'Arora',
+    age: 2037 - 1991,
+    job: 'teacher',
+    friends: ['Michael', 'Steven', 'Peter']
+};
+console.log(jonasObject);
+//Exercise
+const myCountry = {
+    country: 'India',
+    capital: 'New Delhi',
+    language: 'Hindi',
+    population: '1000',
+    neighbours: ['Haryana', 'Uttar Pradesh', 'Punjab']
+};
+console.log(myCountry);
