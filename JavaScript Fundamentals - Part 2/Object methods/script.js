305e22a56409100ecf3763e4694e3d09a482d0bb
username <abhishekarora801@gmail.com>
2022-11-02 18:24:28 +0530

    Adding object methods code

--- JavaScript Fundamentals - Part 2/Object methods/script.js
+++ JavaScript Fundamentals - Part 2/Object methods/script.js
@@ -1,2 +1,45 @@
 'use strict';
 
+const jonas = {
+    firstName: 'Abhishek',
+    lastName: 'Arora',
+    birthYear: 1991,
+    job: 'teacher',
+    friends: ['Michael', 'Peter', 'Steven'],
+    hasDriverLicense: true,
+
+    calcAge: function () {
+        this.age = 2037 - this.birthYear;
+        return this.age;
+    },
+    getSummary: function () {
+        return `${this.firstName} is a ${this.calcAge()}-year old ${this.job}, and he ${this.hasDriverLicense ? 'has a' : 'doesnt have a'} driver's license.`;
+    }
+};
+
+console.log(jonas.getSummary());
+
+//Exercise:
+const myCountry = {
+    country: 'India',
+    capital: 'New Delhi',
+    language: 'Hindi',
+    population: '1000',
+    neighbours: ['Haryana', 'Uttar Pradesh', 'Punjab'],
+    describe: function () {
+        return `${this.country} has ${this.population} million ${this.language}-speaking people, ${this.neighbours.length} neighbouring countries and a capital called ${this.capital}.`;
+    },
+    checkIsland: function () {
+        this.isIsland = this.neighbours.length === 0 ? true : false;
+        return this.isIsland;
+    }
+}
+
+console.log(myCountry.describe());
+console.log(myCountry.checkIsland());
+// console.log(myCountry.isIsland);
+console.log(myCountry);
+
+
+
+
