98476b78197c64145da8debad0b3faf7828dd6bf
username <abhishekarora801@gmail.com>
2022-11-02 19:20:32 +0530

    the for loop

---
+++ JavaScript Fundamentals - Part 2/Iteration The For loop/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// console.log(`Lifting weights repetition 1`);
+
+for (let rep = 1; rep <= 10; rep++) {
+    console.log(`Lifting weights repetition ${rep}`);
+}
+
+
+//Exercise:
+for (let voter = 1; voter <= 50; voter++) {
+    console.log(`Voter number ${voter} is currently voting!`);
+}
