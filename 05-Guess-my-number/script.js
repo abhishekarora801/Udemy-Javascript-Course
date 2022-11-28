6b3532475d6f503cfd26841a57fd4781b4040bd4
username <abhishekarora801@gmail.com>
2022-11-28 16:47:49 +0530

    Implementing Highscores

--- 05-Guess-my-number/script.js
+++ 05-Guess-my-number/script.js
@@ -18,6 +18,8 @@ console.log(document.querySelector(".message").textContent);
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
+let highScore = 0;
+
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   //It Outputs the value to the console, what you've typed there!
@@ -37,6 +39,10 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector("body").style.backgroundColor = "#60b437";
     document.querySelector(".number").style.width = "30rem";
 
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector(".highscore").textContent = highScore;
+    }
     //When guess is too high!
   } else if (guess > secretNumber) {
     if (score > 1) {
