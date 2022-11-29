61aa0e9d4e3fd548f9ff06ea44ff8d00de9c6026
username <abhishekarora801@gmail.com>
2022-11-29 16:19:56 +0530

    Modal game completed

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -1,3 +1,33 @@
 'use strict';
 
 const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  console.log('Button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+//creating function to make code DRY (Don't repeat yourself)
+const closeModal = function () {
+  console.log('Popup closed');
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+//closing pop-up through close button
+btnCloseModal.addEventListener('click', closeModal);
+
+//closing pop-up through clicking outside
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal(); //now we have to call this closeModal fn.
+  }
+});
