802859b3ca6c16cbef471747d5753995de55ab75
username <abhishekarora801@gmail.com>
2022-12-11 16:38:09 +0530

    Understanding the usage of var, let and const

--- 08-Behind-the-scenes/script.js
+++ 08-Behind-the-scenes/script.js
@@ -29,3 +29,46 @@ function calcAge(birthYear) {
 const firstName = 'Abhishek';
 
 calcAge(1991);
+
+//Variables
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Abhishek';
+let job = 'teacher';
+const year = 1991;
+
+//Calling functions before defined
+console.log(addDec(2, 3)); //Works fine!
+// console.log(addExpr(2, 3)); //Uncaught TypeError: addExpr is not a function
+
+//Functions
+function addDec(a, b) {
+  return a + b;
+}
+
+var addExpr = function (a, b) {
+  return a + b;
+};
+
+const addArrow = (a, b) => a + b;
+
+// Example
+if (!numProducts) deleteShoppingCart();
+var numProducts = 10;
+function deleteShoppingCart() {
+  console.log('All products deleted');
+}
+//Conclusion =>
+//Hoisting works with var variables.
+//So even after telling it to only execute on 0 value,
+//it will still execute.
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
