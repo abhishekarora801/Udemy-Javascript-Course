569f8d57520dc950b7a0c1cb2d92f3c86fdfa73f
username <abhishekarora801@gmail.com>
2023-01-03 01:03:02 +0530

    Data destructuring in Array

--- 09-DS & Modern Operators & Strings/script.js
+++ 09-DS & Modern Operators & Strings/script.js
@@ -12,6 +12,9 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
   openingHours: {
     thu: {
       open: 12,
@@ -27,3 +30,34 @@ const restaurant = {
     },
   },
 };
+
+const arr = [2, 3, 4];
+const [x, y, z] = arr; //V.IMPORTANT, called Destructuring!
+console.log(x, y, z);
+//Also, original array not destroyed
+
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+
+//Switching variables
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+// console.log(main, secondary);
+
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+//Using fucntion used above
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+const nested = [2, 4, [5, 6]];
+// const [i, , j] = nested;
+// console.log(i, j);
+const [i, , [j, k]] = nested; //Nested Destructuring!
+console.log(i, j, k);
+
+//Setting default values
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r); //getting r=1 as the default value!
